86517b4867ecb734a83cdd238db033649b546556
Nyeedz <chr.alex@hotmail.com>
2017-03-22 23:28:35 -0300

    Adicionado sql script para criação do banco de dados

--- db.sql
+++ db.sql
@@ -1,45 +1,57 @@
-/*
-Navicat MySQL Data Transfer
+CREATE DATABASE  IF NOT EXISTS `crudnodejs` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `crudnodejs`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: crudnodejs
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.16-MariaDB
 
-Source Server         : localhost
-Source Server Version : 100121
-Source Host           : localhost:3306
-Source Database       : crudnodejs
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-Target Server Type    : MYSQL
-Target Server Version : 100121
-File Encoding         : 65001
+--
+-- Table structure for table `produtos`
+--
 
-Date: 2017-03-10 14:33:47
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for produtos
--- ----------------------------
 DROP TABLE IF EXISTS `produtos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `produtos` (
   `id_produtos` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) NOT NULL,
   `preco` float NOT NULL,
   `estoque` int(11) NOT NULL,
   `data_criacao` datetime DEFAULT NULL,
+  `isDeleted` tinyint(4) DEFAULT '0',
   PRIMARY KEY (`id_produtos`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Table structure for users
--- ----------------------------
-DROP TABLE IF EXISTS 'users';
-CREATE TABLE 'users' (
-  'id' int(11) NOT NULL AUTO_INCREMENT,
-  'nome' varchar(255) NOT NULL,
-  'email' varchar(255) NOT NULL,
-  'password' varchar(255) NOT NULL
-);
+--
+-- Dumping data for table `produtos`
+--
 
--- ----------------------------
--- Records of produtos
--- ----------------------------
-INSERT INTO `produtos` VALUES ('1', 'Banco criado com sucesso', '10', '10', '2017-03-10 14:33:22');
+LOCK TABLES `produtos` WRITE;
+/*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
+INSERT INTO `produtos` VALUES (8,'Melão',7,22,'2017-08-03 22:02:03',0),(9,'Chocolate',12,56,'2017-03-09 04:29:38',0),(38,'ddd',22,2,'2017-03-16 09:22:55',1),(39,'teste',2,2,'2017-03-16 09:28:17',1),(40,'ddd',2,2,'2017-03-16 09:31:48',1),(41,'ddd',222,222,'2017-03-22 11:19:29',1);
+/*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-03-22 23:27:02
